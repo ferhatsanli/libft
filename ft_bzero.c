4344417886a881d51dcdb24ca2d40c81bd29dc63
Ferhat Sanli <fsanli@f1r3s19.codam.nl>
2024-10-09 18:38:46 +0200

    ft_bzero done

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,28 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fsanli <fsanli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/08 20:03:31 by fsanli            #+#    #+#             */
-/*   Updated: 2024/10/09 18:38:14 by fsanli           ###   ########.fr       */
+/*   Created: 2024/10/09 18:35:18 by fsanli            #+#    #+#             */
+/*   Updated: 2024/10/09 18:38:10 by fsanli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <stddef.h>
 
-# include <stddef.h>
+void	ft_bzero(void *s, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_ispunct(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-
-#endif
+	ptr = s;
+	i = 0;
+	while (i < n)
+	{
+		*ptr++ = '\0';
+	}
+}
