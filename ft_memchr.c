912ae480201f8de01386a8b0f02c5d4d7a7fa32b
Ferhat Sanli <fsanli@f0r3s9.codam.nl>
2024-10-22 22:46:35 +0200

    norminette fix

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,13 +6,13 @@
 /*   By: fsanli <fsanli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 22:12:47 by fsanli            #+#    #+#             */
-/*   Updated: 2024/10/22 22:43:03 by fsanli           ###   ########.fr       */
+/*   Updated: 2024/10/22 22:46:17 by fsanli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
 	char	*addr;
